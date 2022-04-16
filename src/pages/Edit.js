945979c104d861e3edc9024ce8957b77917bbc37
Kyle KIM <k2wackie@gmail.com>
2022-04-16 15:31:53 +0900

    Using useParams, useSearchParams & useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,36 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [serchParams, setSearchParams] = useSearchParams();
+
+  const id = serchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = serchParams.get("mode");
+  console.log("mode: ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "winterload" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
